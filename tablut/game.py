9865b64815cd3291c903787e81e5b819adc0c274
n28div <nicolaslazzari98@gmail.com>
2020-11-28 02:17:47 +0100

    implemented simulation of a move

--- tablut/game.py
+++ tablut/game.py
@@ -1,9 +1,16 @@
 from enum import Enum
+from copy import deepcopy
 from tablut.board import WinException, DrawException, LoseException
 
 class Player(Enum):
     WHITE = "W"
     BLACK = "B"
+
+    def next(self):
+        if self is self.WHITE:
+            return self.BLACK
+        else:
+            return self.WHITE
 
 
 class TurnException(Exception):
@@ -18,17 +25,28 @@ class Game(object):
     def __init__(self, board):
         self.board = board
         self.turn = Player.WHITE
+        self.ended = False
+        self.winner = None
 
     def white_move(self, start, end):
         """
         Make the white move
         """
-        if self.turn == Player.WHITE:
+        if self.turn == Player.WHITE and not self.ended:
             try:
                 self.board.step(Player.WHITE, start, end)
                 self.turn = Player.BLACK
-            except (WinException, DrawException, LoseException):
-                raise
+            except WinException:
+                # white winned
+                self.ended = True
+                self.winner = Player.WHITE
+            except LoseException:
+                # white losed (shouldn't happen after a white move right now)
+                # FIXME: Stuck player? 
+                self.ended = True
+                self.winner = Player.BLACK
+            except DrawException:
+                self.ended = True
             except Exception as e:
                 raise ValueError("White move illegal: %s" % str(e))
         else:
@@ -42,9 +60,34 @@ class Game(object):
             try:
                 self.board.step(Player.BLACK, start, end)
                 self.turn = Player.WHITE
-            except (WinException, DrawException, LoseException):
-                raise
+            except WinException:
+                # white winned (shouldn't happen here...)
+                # FIXME: Implement stuck player?
+                self.ended = True
+                self.winner = Player.WHITE
+            except LoseException:
+                # white losed
+                self.ended = True
+                self.winner = Player.BLACK
+            except DrawException:
+                self.ended = True
             except Exception as e:
                 raise ValueError("Black move illegal:%s " % str(e))
         else:
             raise TurnException("Its white player turn")
+
+    def what_if(self, start, end, player=None):
+        """
+        Return the game instance if a particular move is made but doesnt modify the actual instance. 
+        The right player is automatically used if not provided
+        """
+        if player is None:
+            player = self.turn
+
+        cpy = deepcopy(self)
+        if cpy.turn is Player.WHITE:
+            cpy.white_move(start, end)
+        else:
+            cpy.black_move(start, end)
+
+        return cpy        
