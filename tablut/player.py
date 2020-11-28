942a084dc1bdadb73f7490d3dac43f39d8d30286
n28div <nicolaslazzari98@gmail.com>
2020-11-28 02:17:37 +0100

    less overengineered random player

--- tablut/player.py
+++ tablut/player.py
@@ -11,35 +11,25 @@ class RandomPlayer(object):
     def __init__(self, game, player):    
         self.game = game
         self.player = player
-        self.stop = False
-
-        self.thread = threading.Thread(target=self._runtime)
-        self.thread.start()
-
+        
     def _random_move(self):
         start = (randint(0, 8), randint(0, 8))
         end = (randint(0, 8), randint(0, 8))
         return start, end
 
-    def _runtime(self):
-        while not self.stop:
-            while self.game.turn is not self.player:
-                pass
-
-            # Find a random move
+    def play(self):
+        # Find a random move
+        start, end = self._random_move()
+        legal, _ = self.game.board.is_legal(self.player, start, end)
+        while not legal:
             start, end = self._random_move()
             legal, _ = self.game.board.is_legal(self.player, start, end)
-            while not legal:
-                start, end = self._random_move()
-                legal, _ = self.game.board.is_legal(self.player, start, end)
 
-            try:
-                if self.player is Player.WHITE:
-                    self.game.white_move(start, end)
-                elif self.player is Player.BLACK:
-                    self.game.black_move(start, end)
-            except ValueError:
-                # illegal move... shouldnt happen
-                pass
-            except (WinException, LoseException, DrawException):
-                self.stop = True
+        try:
+            if self.player is Player.WHITE:
+                self.game.white_move(start, end)
+            elif self.player is Player.BLACK:
+                self.game.black_move(start, end)
+        except (ValueError, WinException, LoseException, DrawException):
+            # illegal move... shouldnt happen
+            pass
